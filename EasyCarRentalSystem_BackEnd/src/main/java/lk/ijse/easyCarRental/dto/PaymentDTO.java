221ba678b4e9e9f1a2945a30ca2f84bf60469707
sajithmadusamkha <sajithmadusankha02@gmail.com>
2023-03-01 12:31:22 +0530

    content updated

--- EasyCarRentalSystem_BackEnd/src/main/java/lk/ijse/easyCarRental/dto/PaymentDTO.java
+++ EasyCarRentalSystem_BackEnd/src/main/java/lk/ijse/easyCarRental/dto/PaymentDTO.java
@@ -1,4 +1,24 @@
 package lk.ijse.easyCarRental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class PaymentDTO {
+    private String paymentId;
+    private LocalDate date;
+    private double amount;
+    private double damageCost;
+    private String damageDescription;
+    private long extraMileage;
+    private double costPerExtraKm;
+    private String paymentStratus;
+    private String rentalId;
 }
--- EasyCarRentalSystem_BackEnd/src/main/java/lk/ijse/easyCarRental/service/impl/RentalServiceImpl.java
+++ EasyCarRentalSystem_BackEnd/src/main/java/lk/ijse/easyCarRental/service/impl/RentalServiceImpl.java
@@ -1,4 +1,9 @@
 package lk.ijse.easyCarRental.service.impl;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class RentalServiceImpl {
 }
