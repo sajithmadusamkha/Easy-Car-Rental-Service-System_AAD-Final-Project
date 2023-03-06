e0b23d1cc42fd7bf2b4feb7ab72db95002773e06
sajithmadusamkha <sajithmadusankha02@gmail.com>
2023-03-06 11:57:37 +0530

    content updated

--- EasyCarRentalSystem_BackEnd/src/main/java/lk/ijse/easyCarRental/controller/PaymentController.java
+++ EasyCarRentalSystem_BackEnd/src/main/java/lk/ijse/easyCarRental/controller/PaymentController.java
@@ -1,11 +1,23 @@
 package lk.ijse.easyCarRental.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.easyCarRental.dto.PaymentDTO;
+import lk.ijse.easyCarRental.service.PaymentService;
+import lk.ijse.easyCarRental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/payment")
 @CrossOrigin
 public class PaymentController {
+
+    @Autowired
+    PaymentService service;
+
+    @PostMapping
+    public ResponseUtil savePayment(@RequestBody PaymentDTO dto) {
+        service.savePayment(dto);
+        return new ResponseUtil("200",dto.getPaymentId()+ " Paid...!",null);
+    }
+
 }
